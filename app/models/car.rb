4303a12c636851ee67cb79669cc3d52c94341bc6
Gabriel Chein <gabrielbcmuniz@gmail.com>
2024-06-04 19:55:15 -0300

    Removed the association for Listings that was left on the Car model

--- app/models/car.rb
+++ app/models/car.rb
@@ -2,7 +2,6 @@ class Car < ApplicationRecord
   belongs_to :owner, class_name: "User"
 
   has_many :bookings, dependent: :destroy
-  has_many :listings, dependent: :destroy
 
   validates :brand, :model, :license_plate, :daily_price, presence: true
 end
