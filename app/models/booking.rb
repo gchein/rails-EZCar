7775f1902398ac08839f5fdee03857a6d3b49d70
Gabriel Chein <gabrielbcmuniz@gmail.com>
2024-06-05 13:25:40 -0300

    Added the user profile page. Also deleted the pointless 'cars/create' view

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,4 +1,8 @@
 class Booking < ApplicationRecord
   belongs_to :car
   belongs_to :renter, class_name: "User"
+
+  def duration
+    (end_date - start_date).to_i
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -9,4 +9,5 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "posts#index"
   resources :cars
+  get 'profile', to: 'users#profile'
 end
