b751b77d79da7c6ef71c8acf14f3f8a92e840aa7
leo cotello <loctl97@gmail.com>
2024-06-06 18:43:31 -0300

    I created a feature that displays the status of the renter's car.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
+
+  def rental_status(status)
+
+    if status == 'archived'
+      return 'btn btn-danger'
+    elsif status == 'active'
+      return 'btn btn-success'
+    else
+      return 'btn btn-warning'
+    end
+  end
 end
