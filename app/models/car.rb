65b5a1eabbefe72baf2ef642d7669628958294dd
Gabriel Coelho <gabrielcoelhocn08@gmail.com>
2024-06-06 12:15:05 -0300

    Created search bars for cars (index and new)

--- app/models/car.rb
+++ app/models/car.rb
@@ -3,7 +3,7 @@ class Car < ApplicationRecord
 
   has_many :bookings, dependent: :destroy
 
-  validates :brand, :model, :license_plate, :daily_price, presence: true
+  validates :brand, :model, :license_plate, :daily_price, :address, presence: true
 
   geocoded_by :address
   after_validation :geocode, if: :will_save_change_to_address?
